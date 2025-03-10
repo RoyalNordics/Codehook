1a6d85a552152678194a3f1bbafdb3f865e14f2f
Thomas Flyttov Møller <thomas.flyttov@hotmail.com>
2025-03-10 13:08:26 +0000

    Fix CORS issue for browser requests

--- main.py
+++ main.py
@@ -38,3 +38,14 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Fix CORS issue
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (Change to frontend URL for security)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
